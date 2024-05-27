656f4057a86be98795119b0c1c39bc8e81d94105
Stavya3 <Stavyakc03@gmail.com>
2024-05-27 15:22:20 +0530

    Cursor magnifies with a border when hovering over the h4 of the navbar

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ document.addEventListener("mousemove", function (dets) {
   blur.style.top = dets.y - 200 + "px";
 });
 
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function(elem){
+  elem.addEventListener("mouseenter", function(){
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function(){
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95c11e";
+    crsr.style.backgroundColor = "#95c11e";
+  })
+})
+
 gsap.to("#nav", {
   backgroundColor: "#000",
   height: "110px",
